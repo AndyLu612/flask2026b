3c9cf72fd85efab939361c7a94f651c8c29c3149
Andy <87714954+AndyLu612@users.noreply.github.com>
2026-03-19 12:23:24 +0800

    flask

---
+++ py/example1.py
@@ -0,0 +1,9 @@
+def split(x):
+	x = x.split(",")
+	school = x[0].replace("我是","")
+	print(f"學校:{school}")
+	print(f"姓名:{x[2]}")
+
+if __name__ == "__main__":
+	Name = "我是靜宜大學,資管四A,盧安毅"
+	split(Name)
---
+++ py/example2.py
@@ -0,0 +1,4 @@
+from example1 import split
+
+Name = "我是靜宜大學,資管四A,Andy Lu"
+split(Name)
