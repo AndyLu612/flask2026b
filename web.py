5ced42e82f97c2b8aacc67dd3b665b1a7bde808f
Andy <87714954+AndyLu612@users.noreply.github.com>
2026-05-28 12:04:18 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 from google import genai
+from google.genai import types
 
 from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime
@@ -692,7 +693,28 @@ def webhook4():
                 info = "很抱歉，目前無符合這個關鍵字的電影"
     
     elif (action == "input.unknown"):
-        info =  req["queryResult"]["queryText"]
+
+        instruction_text = (
+            "你是一個熱心且知識豐富的專業智慧助理。"
+            "對於使用者的提問，請回覆重點的關鍵字，不要重述問題。"         
+        )
+
+
+        ai_config = types.GenerateContentConfig(
+            max_output_tokens=500, 
+            system_instruction=instruction_text
+        )
+
+        response = client.models.generate_content(
+            model='gemini-3.5-flash', 
+            contents=req["queryResult"]["queryText"],
+            config=ai_config,
+        )
+
+        if response.text:
+            info = response.text
+        else:
+            info = "抱歉，我現在無法生成回應，請稍後再試。"
 
 
     return jsonify({
